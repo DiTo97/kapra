9f82b1db15927eb55e7c34efc295628520df7968
DiTo97 <fede97.minutoli@gmail.com>
2021-03-02 01:50:40 +0100

    Polished Experiments.ipynb

--- k_P_anonymity.py
+++ k_P_anonymity.py
@@ -3,12 +3,14 @@
 Supporting Pattern-preserving Anonymization for Time-series Data
 """
 
-from includes.pattern_loss import global_pattern_loss
-from includes.metric import global_anon_value_loss
-from includes.pattern_loss import generate_output_path
 import sys
+import time
+import os
+
+import pandas as pd
 
 from loguru import logger
+from pathlib import Path
 
 # Custom imports #
 from includes.naive import Naive
@@ -16,18 +18,18 @@ from includes.kapra import KAPRA
 
 from includes.io import usage
 
-import time
-import os
+from includes.metric import global_anon_value_loss
 
-import pandas as pd
+from includes.pattern_loss import global_pattern_loss
+from includes.pattern_loss import generate_output_path
 
-from pathlib import Path
+RES_DIR = 'results'
 
 if __name__ == "__main__":
     if not len(sys.argv) == 7:
         usage()
 
-    # Parse arguments
+    # 1. Parse arguments
     algorithm = sys.argv[1].lower()
 
     k_value = int(sys.argv[2])
@@ -41,7 +43,7 @@ if __name__ == "__main__":
         logger.error('<k_value> must be greater or equal than <P_value>')
         usage()
     
-
+    # 2. Execute (k, P) algorithm
     start = time.time()
 
     if algorithm == 'naive':
@@ -54,28 +56,50 @@ if __name__ == "__main__":
         usage()
 
     end = time.time()
+    eta = round(float(end - start), 3) # Elapsed time
 
-    os.makedirs("results", exist_ok=True) 
+    # 3. Create results dir, if non-existent
+    abs_root_path = Path(__file__).absolute().parent
+    os.makedirs(abs_root_path / RES_DIR, exist_ok=True) 
+
+    # 4. Compute pattern loss (PL)
+    logger.info('Computing pattern loss...')
 
     global_ploss, global_ploss_avg = global_pattern_loss(data_path)
 
-    anonym_path = generate_output_path(data_path)
+    tot_pattern_loss = round(float(global_ploss), 3)
+    avg_pattern_loss = round(float(global_ploss_avg), 3)
 
+    logger.info('Computed pattern loss of ' + str(avg_pattern_loss))
+
+    # 5. Compute instant value loss (VL)
+    logger.info('Computing instant value loss...')
+
+    anonym_path = generate_output_path(data_path)
     glob_vl, mean_vl = global_anon_value_loss(anonym_path)
 
-    print("Elapsed time in seconds : " + str(end - start) + "")
+    tot_value_loss = round(float(glob_vl), 3)
+    avg_value_loss = round(float(mean_vl), 3)
 
-    results = pd.DataFrame(columns =['ElapsedTime', 'GlobalPatternLoss', 'AveragePatternLoss', 'GlobalValueLoss', 'MeanValueLoss']) 
+    logger.info('Computed instant value loss of ' + str(avg_value_loss))
+
+    # 6. Save results as CSV file
+    results_df = pd.DataFrame(columns = [ 'eta',
+            'tot_pattern_loss', 'avg_pattern_loss',
+            'tot_value_loss', 'avg_value_loss' ]) 
   
-
-    results.loc[0]=[float(end-start), float(global_ploss), float(global_ploss_avg), \
-        float(glob_vl), float(mean_vl)]
+    results_df.loc[0] = [ eta,
+            tot_pattern_loss, avg_pattern_loss,
+            tot_value_loss, avg_value_loss ]
 
     abs_data_path = Path(data_path).absolute()
 
-    outfilename = abs_data_path.parts[-1].replace('.csv', '') + "_" + algorithm + "_k" + str(k_value) + "_p" \
-        + str(P_value) + "_paa" + str(paa_value) + "_l" + str(l_value) + ".csv"
+    outfilename = abs_data_path.parts[-1].replace('.csv', '') \
+            + '_' + algorithm + '_k' + str(k_value)           \
+            + '_P' + str(P_value) + '_paa' + str(paa_value)   \
+            + '_l' + str(l_value) + '.csv'
 
-    outfilepath = "./results/" + outfilename
+    outfilepath = abs_root_path / RES_DIR / outfilename
+    results_df.to_csv(outfilepath, sep =',', index=False) 
 
-    results.to_csv(outfilepath, sep ='\t') 
+    print('\nFinalized (k, P) algorithm - ETA: ' + str(eta) + ' sec')
