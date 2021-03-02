bf109a5a4dcae3386ac2aebcca8d73e60dc755c3
DiTo97 <fede97.minutoli@gmail.com>
2021-03-02 19:47:24 +0100

    Created experiments.py

--- experiments/test_scalability.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#
-# Please note: This test is still in its tentative stage, since as of yet there's no evidence of appreciatable responses
-# to varying scalability from both naive and KAPRA implementations of (k, P)-anonymity.
